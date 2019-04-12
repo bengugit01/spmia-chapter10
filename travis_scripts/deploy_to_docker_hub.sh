57f70a645ce61cc0f21a0f3b1d7705ce860ae55f
Ben <ben.gu.au@gmail.com>
2019-04-13 09:38:04 +1000

    replace bengugit01 with bengu in the docker image

--- travis_scripts/deploy_to_docker_hub.sh
+++ travis_scripts/deploy_to_docker_hub.sh
@@ -1,8 +1,8 @@
 echo "Pushing service docker images to docker hub ...."
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push bengugit01/tmx-authentication-service:$BUILD_NAME
-docker push bengugit01/tmx-licensing-service:$BUILD_NAME
-docker push bengugit01/tmx-organization-service:$BUILD_NAME
-docker push bengugit01/tmx-confsvr:$BUILD_NAME
-docker push bengugit01/tmx-eurekasvr:$BUILD_NAME
-docker push bengugit01/tmx-zuulsvr:$BUILD_NAME
+docker push bengu/tmx-authentication-service:$BUILD_NAME
+docker push bengu/tmx-licensing-service:$BUILD_NAME
+docker push bengu/tmx-organization-service:$BUILD_NAME
+docker push bengu/tmx-confsvr:$BUILD_NAME
+docker push bengu/tmx-eurekasvr:$BUILD_NAME
+docker push bengu/tmx-zuulsvr:$BUILD_NAME
