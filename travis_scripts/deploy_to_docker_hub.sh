537c1ce3b736fa2798d30447007e59dac3aedd9c
Ben <ben.gu.au@gmail.com>
2019-04-12 20:27:26 +1000

    Use bengugit01 instead of johncarnell's github account

--- travis_scripts/deploy_to_docker_hub.sh
+++ travis_scripts/deploy_to_docker_hub.sh
@@ -1,8 +1,8 @@
 echo "Pushing service docker images to docker hub ...."
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push johncarnell/tmx-authentication-service:$BUILD_NAME
-docker push johncarnell/tmx-licensing-service:$BUILD_NAME
-docker push johncarnell/tmx-organization-service:$BUILD_NAME
-docker push johncarnell/tmx-confsvr:$BUILD_NAME
-docker push johncarnell/tmx-eurekasvr:$BUILD_NAME
-docker push johncarnell/tmx-zuulsvr:$BUILD_NAME
+docker push bengugit01/tmx-authentication-service:$BUILD_NAME
+docker push bengugit01/tmx-licensing-service:$BUILD_NAME
+docker push bengugit01/tmx-organization-service:$BUILD_NAME
+docker push bengugit01/tmx-confsvr:$BUILD_NAME
+docker push bengugit01/tmx-eurekasvr:$BUILD_NAME
+docker push bengugit01/tmx-zuulsvr:$BUILD_NAME
