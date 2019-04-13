e7504bd7a5b1a5c6a6f0bbfd7a764c6eb0e1a8b3
Ben <ben.gu.au@gmail.com>
2019-04-14 09:23:12 +1000

    added profile_name="spmia-example" and --cluster-config ${profile_name}

--- travis_scripts/deploy_to_amazon_ecs.sh
+++ travis_scripts/deploy_to_amazon_ecs.sh
@@ -1,9 +1,18 @@
 echo "Launching $BUILD_NAME IN AMAZON ECS"
 #ecs-cli configure --region ap-southeast-2 --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY --cluster spmia-tmx-dev
 
-ecs-cli configure profile --profile-name edXProjectUser --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY
+profile_name="spmia-example"
 
-ecs-cli configure --region ap-southeast-2  --cluster spmia-tmx-dev --default-launch-type FARGATE
+echo "[*] [$( date +'%H:%M:%S')] Configure ECS profile..."
+ecs-cli configure profile --profile-name ${profile_name} --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY
 
-ecs-cli compose --file docker/common/docker-compose.yml up
+echo "[*] [$( date +'%H:%M:%S')] Configure ECS cluster before launch..."
+ecs-cli configure --region ap-southeast-2  --cluster spmia-tmx-dev --default-launch-type EC2 --config-name ${profile_name} 		
+			
+echo "[*] [$( date +'%H:%M:%S')] Bring up EC2 instance..."
+# bring up cluster
+ecs-cli up  --cluster-config ${profile_name} --ecs-profile ${profile_name}
+
+echo "[*] [$( date +'%H:%M:%S')] Create Services in ECS cluster as defined in docker-compose.yml..."
+ecs-cli compose --file docker/common/docker-compose.yml --cluster-config ${profile_name} up
 rm -rf ~/.ecs
