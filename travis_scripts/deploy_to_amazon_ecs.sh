c396bfdd8f229e2607aa5cfc44ac1c8d129d6765
Ben <ben.gu.au@gmail.com>
2019-04-13 14:48:28 +1000

    Try o fix the error as reported below:
    $ sh travis_scripts/deploy_to_amazon_ecs.sh
    Launching chapter10-master-20190413042145-5 IN AMAZON ECS
    Incorrect Usage. flag provided but not defined: -access-key
    NAME:
       ecs-cli configure - Stores a single cluster configuration.
    USAGE:
       ecs-cli configure command [command options] [arguments...]
    COMMANDS:
         profile  Stores a single profile.
         default  Sets the default cluster config.
         migrate  Migrates a legacy ECS CLI configuration file to the current YAML format.

--- travis_scripts/deploy_to_amazon_ecs.sh
+++ travis_scripts/deploy_to_amazon_ecs.sh
@@ -1,4 +1,9 @@
 echo "Launching $BUILD_NAME IN AMAZON ECS"
-ecs-cli configure --region ap-southeast-2 --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY --cluster spmia-tmx-dev
+#ecs-cli configure --region ap-southeast-2 --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY --cluster spmia-tmx-dev
+
+ecs-cli configure profile --profile-name edXProjectUser --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY
+
+ecs-cli configure --region ap-southeast-2  --cluster spmia-tmx-dev
+
 ecs-cli compose --file docker/common/docker-compose.yml up
 rm -rf ~/.ecs
