79d519848e406dbe4e9770b663ec3382b993ade2
Ben <ben.gu.au@gmail.com>
2019-04-14 09:48:14 +1000

    Commented out ecs-cli up

--- travis_scripts/deploy_to_amazon_ecs.sh
+++ travis_scripts/deploy_to_amazon_ecs.sh
@@ -9,9 +9,9 @@ ecs-cli configure profile --profile-name ${profile_name} --access-key $AWS_ACCES
 echo "[*] [$( date +'%H:%M:%S')] Configure ECS cluster before launch..."
 ecs-cli configure --region ap-southeast-2  --cluster spmia-tmx-dev --default-launch-type EC2 --config-name ${profile_name} 		
 			
-echo "[*] [$( date +'%H:%M:%S')] Bring up EC2 instance..."
+#echo "[*] [$( date +'%H:%M:%S')] Bring up EC2 instance..."
 # bring up cluster
-ecs-cli up  --cluster-config ${profile_name} --ecs-profile ${profile_name}
+#ecs-cli up  --cluster-config ${profile_name} --ecs-profile ${profile_name}
 
 echo "[*] [$( date +'%H:%M:%S')] Create Services in ECS cluster as defined in docker-compose.yml..."
 ecs-cli compose --file docker/common/docker-compose.yml --cluster-config ${profile_name} up
