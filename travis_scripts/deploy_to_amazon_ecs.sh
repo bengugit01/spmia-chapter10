11b4c65b82afc40a6986b4f536df96e2cc28c28b
Ben <ben.gu.au@gmail.com>
2019-04-13 15:24:33 +1000

    Try --default-launch-type FARGATE

--- travis_scripts/deploy_to_amazon_ecs.sh
+++ travis_scripts/deploy_to_amazon_ecs.sh
@@ -3,7 +3,7 @@ echo "Launching $BUILD_NAME IN AMAZON ECS"
 
 ecs-cli configure profile --profile-name edXProjectUser --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY
 
-ecs-cli configure --region ap-southeast-2  --cluster spmia-tmx-dev
+ecs-cli configure --region ap-southeast-2  --cluster spmia-tmx-dev --default-launch-type FARGATE
 
 ecs-cli compose --file docker/common/docker-compose.yml up
 rm -rf ~/.ecs
