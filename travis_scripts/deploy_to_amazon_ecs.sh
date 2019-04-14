ba7e8982143fb365ce0bc5d7cae7d59e11d527cb
Ben <ben.gu.au@gmail.com>
2019-04-14 10:50:54 +1000

    use ecsInstanceRole

--- travis_scripts/deploy_to_amazon_ecs.sh
+++ travis_scripts/deploy_to_amazon_ecs.sh
@@ -2,14 +2,9 @@ echo "Launching $BUILD_NAME IN AMAZON ECS"
 #ecs-cli configure --region ap-southeast-2 --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY --cluster spmia-tmx-dev
 
 profile_name="spmia-example"
-role_name="ECSExampleRole"
 
-echo "[*] [$( date +'%H:%M:%S')] Creating IAM role for ECS instances... (gives you possibility to use your ECR - AWS private registry for example"
-create_role_response=$( aws iam create-role --role-name ${role_name} --assume-role-policy-document file://ecs-policy.json --description "ECS Cluster default role" )
-put_role_policy_response=$( aws iam put-role-policy --role-name ${role_name} --policy-name ecsMediumRolePolicy --policy-document file://ecs-role.json ) 
-
-create_instance_profile_response=$( aws iam create-instance-profile --instance-profile-name ecsInstanceProfileMedium )
-add_role_to_instance_response=$( aws iam add-role-to-instance-profile --instance-profile-name ecsInstanceProfileMedium --role-name ${role_name} )
+# use role as created by AWS during manual cluster creation 
+role_name="ecsInstanceRole"
 
 echo "[*] [$( date +'%H:%M:%S')] Configure ECS profile..."
 ecs-cli configure profile --profile-name ${profile_name} --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY
