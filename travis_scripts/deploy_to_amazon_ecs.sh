5e3669694ef1ee7c6070703cbe8f7aca8b572a8b
Ben <ben.gu.au@gmail.com>
2019-04-16 15:55:40 +1000

    docker-compose.yml:   upgrade from version 1 to version 3 deploy_to_amazon_ecs.sh:    added various environment variables    added service to ecs-cli compose command to start the ECS service    etc.

--- travis_scripts/deploy_to_amazon_ecs.sh
+++ travis_scripts/deploy_to_amazon_ecs.sh
@@ -1,21 +1,33 @@
 echo "Launching $BUILD_NAME IN AMAZON ECS"
 #ecs-cli configure --region ap-southeast-2 --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY --cluster spmia-tmx-dev
 
-profile_name="spmia-example"
+profile_name="spmia-dev"
+cluster_name=${profile_name} + "-cluster"
 
 # use role as created by AWS during manual cluster creation 
 role_name="ecsInstanceRole"
+tier_class="t2.large"
+keypair="mykey"
+service_name=${profile_name} + '-service'
 
 echo "[*] [$( date +'%H:%M:%S')] Configure ECS profile..."
 ecs-cli configure profile --profile-name ${profile_name} --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY
 
 echo "[*] [$( date +'%H:%M:%S')] Configure ECS cluster before launch..."
-ecs-cli configure --region ap-southeast-2  --cluster spmia-tmx-dev --default-launch-type EC2 --config-name ${profile_name} 		
+ecs-cli configure --region ap-southeast-2  --cluster ${cluster_name} --default-launch-type EC2 --config-name ${profile_name} 		
 			
 #echo "[*] [$( date +'%H:%M:%S')] Bring up EC2 instance..."
 # bring up cluster
 #ecs-cli up  --cluster-config ${profile_name} --ecs-profile ${profile_name} --instance-role ${role_name}
 
+echo ""
+echo "[*] [$( date +'%H:%M:%S')] Bring up EC2 instance..."
+# bring up cluster
+ecs-cli up  --instance-type ${tier_class} --cluster-config ${cluster_name}  --instance-role ${role_name} --keypair ${keypair} --ecs-profile ${profile_name}
+
+echo ""
 echo "[*] [$( date +'%H:%M:%S')] Create Services in ECS cluster as defined in docker-compose.yml..."
-ecs-cli compose --file docker/common/docker-compose.yml --cluster-config ${profile_name} up
+ecs-cli compose --file docker/common/docker-compose.yml --cluster-config ${profile_name} --project-name ${service_name} service up
 rm -rf ~/.ecs
+
+
